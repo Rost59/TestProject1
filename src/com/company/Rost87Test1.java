97f77118404b9b3fba92997356afa68bf15dcab0
Rost87 <34788968+Rost87@users.noreply.github.com>
2021-01-28 17:33:47 +0500

    Добавлен sout в Main.java. Добавлен новый класс Rost87Test1.java

---
+++ src/com/company/Rost87Test1.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Rost87Test1 {
+    // комментарий добавлен Rost87
+    public static void main(String[] args) {
+        System.out.println("Rost87. Вывод на экран ");
+    }
+}
